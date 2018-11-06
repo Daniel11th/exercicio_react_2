63803aa27ccf24b9dc34d905768afc02735c4c98
Daniel Oliveira <daniel11th@hotmail.com>
2018-11-06 17:32:31 -0200

    primeiro commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack')
+
+module.exports = {
+
+    entry: './ex/index.jsx',
+    output: {
+        path: __dirname + '/public',
+        filename: './bundle.js'
+    },
+    devserver: {
+        port: 8080,
+        contentBase: './public.js',
+    },
+    module: {
+        loaders: [{
+
+            test: /.jsx?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    }
+}
